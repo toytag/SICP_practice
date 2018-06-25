f87d8d085899b0ba241ab00ecd746369b3800c45
tzz <zzt1201@gmail.com>
2018-06-25 19:12:43 +0800

    internal definition

--- Newton-sqrt.scm
+++ Newton-sqrt.scm
@@ -12,4 +12,19 @@
 (define (sqrt x)
     (sqrt-iter 1.0 x))
 
-; (display (sqrt 4))
+(display (sqrt 4))
+
+; ----------------------------------------
+
+(define (sqrt x)
+    (define (improve guess x)
+        (/ (+ guess (/ x guess)) 2))
+    (define (good-enough? guess x)
+        (< (abs (- x (* guess guess))) 0.001))
+    (define (sqrt-iter guess x)
+        (if (good-enough? guess x)
+            guess
+            (sqrt-iter (improve guess x) x)))
+    (sqrt-iter 1.0 x))
+
+(display (sqrt 4))
