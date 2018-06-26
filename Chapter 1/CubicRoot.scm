8cbafebefde7a369586eef6624a39c03a499260f
tzz <zzt1201@gmail.com>
2018-06-26 22:28:53 +0800

    newton's method of aquiring cubic root

---
+++ Chapter 1/CubicRoot.scm
@@ -0,0 +1,13 @@
+(define (cubic-root x)
+    (define (good-enough? guess)
+        (< (abs (- x (* guess guess guess))) 0.001))
+    (define (improve guess)
+        (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+    (define (cubic-root-iter guess)
+        (if (good-enough? guess)
+            guess
+            (cubic-root-iter (improve guess))))
+    (cubic-root-iter 1.0))
+
+(display (cubic-root 8))
+(newline)
