9821e75ddc450951c90736a78885c1ae8c537e64
tzz <zzt1201@gmail.com>
2018-06-27 17:29:16 +0800

    fib-iter

--- Chapter 1/Fibonacii.scm
+++ Chapter 1/Fibonacii.scm
@@ -4,5 +4,17 @@
           (else (+ (fib (- n 1))
                    (fib (- n 2))))))
 
-(display (fib 5))
+(display (fib 40))
+(newline)
+
+; --------------------------------------
+
+(define (fib n)
+    (define (fib-iter pre pre-pre count)
+        (if (= count n)
+            pre
+            (fib-iter (+ pre pre-pre) pre (+ count 1))))
+    (fib-iter 1 0 1))
+
+(display (fib 40))
 (newline)
